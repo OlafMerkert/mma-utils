eb44d0cd4f3ef2234f7a1df529cc0b6c0ff38dfd
Olaf Merkert <olaf@m-merkert.de>
2014-05-28 20:15:22 +0200

    Comparison with polynomial growth in loglog plots

---
+++ plotting.m
@@ -0,0 +1,13 @@
+(* ::Package:: *)
+
+(* ::Text:: *)
+(*For comparing the growth rates with polynomial growth of order 1 to n, we produce a standard loglog plot in the range from 1 to bound.*)
+
+
+GrowthRates=Function[{n,bound},
+Module[{colours,functions},
+colours=Table[Hue[i/n],{i,0,n-1}];
+functions=Table[x^i,{i,1,n}];
+LogLogPlot[
+functions,{x,1,bound},
+PlotStyle->colours]]];
